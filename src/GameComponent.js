62a9ef22ab65ae2ae4e232c064b9b9b6997dd681
andrii <vovchenkoandrii@gmail.com>
2023-02-01 16:30:21 -0800

    added console to Game comp

--- src/GameComponent.js
+++ src/GameComponent.js
@@ -10,6 +10,7 @@ const GameComponent = ({level}) => {
     const [score, setScore] = React.useState(0);
 
     const validateLevelTime = () => {
+        console.log('andrii change');
         switch(level){
             case 'Easy': return 500;
             case 'Medium': return 350;
